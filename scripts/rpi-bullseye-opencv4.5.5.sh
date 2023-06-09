0b45eaa0fa2b1e1750c636f2e5a94576b0a8ee3f
Migz <gm.tapallamiguel@gmail.com>
2023-06-09 13:53:50 +0800

    Updated /scripts files

--- scripts/rpi-bullseye-opencv4.5.5.sh
+++ scripts/rpi-bullseye-opencv4.5.5.sh
@@ -1,48 +1,50 @@
 #!/bin/bash
 set -e
 echo "Installing OpenCV 4.6.0 on your Raspberry Pi 64-bit OS"
-echo "It will take minimal 2.0 hour !"
+echo "It will take atleast 2 hours!"
 cd ~
-# install the dependencies
-sudo apt-get install -y build-essential cmake git unzip pkg-config
-sudo apt-get install -y libjpeg-dev libtiff-dev libpng-dev
-sudo apt-get install -y libavcodec-dev libavformat-dev libswscale-dev
-sudo apt-get install -y libgtk2.0-dev libcanberra-gtk* libgtk-3-dev
-sudo apt-get install -y libgstreamer1.0-dev gstreamer1.0-gtk3
-sudo apt-get install -y libgstreamer-plugins-base1.0-dev gstreamer1.0-gl
-sudo apt-get install -y libxvidcore-dev libx264-dev
-sudo apt-get install -y python3-dev python3-numpy python3-pip
-sudo apt-get install -y libtbb2 libtbb-dev libdc1394-22-dev
-sudo apt-get install -y libv4l-dev v4l-utils
-sudo apt-get install -y libopenblas-dev libatlas-base-dev libblas-dev
-sudo apt-get install -y liblapack-dev gfortran libhdf5-dev
-sudo apt-get install -y libprotobuf-dev libgoogle-glog-dev libgflags-dev
-sudo apt-get install -y protobuf-compiler
+
+# Installing dependencies
+sudo apt install -y build-essential cmake git unzip pkg-config
+sudo apt install -y libjpeg-dev libtiff-dev libpng-dev
+sudo apt install -y libavcodec-dev libavformat-dev libswscale-dev
+sudo apt install -y libgtk2.0-dev libcanberra-gtk* libgtk-3-dev
+sudo apt install -y libgstreamer1.0-dev gstreamer1.0-gtk3
+sudo apt install -y libgstreamer-plugins-base1.0-dev gstreamer1.0-gl
+sudo apt install -y libxvidcore-dev libx264-dev
+sudo apt install -y python3-dev python3-numpy python3-pip
+sudo apt install -y libtbb2 libtbb-dev libdc1394-22-dev
+sudo apt install -y libv4l-dev v4l-utils
+sudo apt install -y libopenblas-dev libatlas-base-dev libblas-dev
+sudo apt install -y liblapack-dev gfortran libhdf5-dev
+sudo apt install -y libprotobuf-dev libgoogle-glog-dev libgflags-dev
+sudo apt install -y protobuf-compiler
 sudo sed -ie 's/CONF_SWAPSIZE=100/CONF_SWAPSIZE=3000/' /etc/dphys-swapfile
 sudo /etc/init.d/dphys-swapfile stop
 sudo /etc/init.d/dphys-swapfile start
 
-
-# download the latest version
+# Downloading latest version
 cd ~ 
 sudo rm -rf opencv*
 wget -O opencv.zip https://github.com/opencv/opencv/archive/4.6.0.zip 
 wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/4.6.0.zip
 unzip opencv.zip 
 unzip opencv_contrib.zip 
-# some administration to make live easier later on
+
+# Administrator privilages to make life easier
 mv opencv-4.6.0 opencv
 mv opencv_contrib-4.6.0 opencv_contrib
-# clean up the zip files
+
+# Cleaning up .zip files
 rm opencv.zip
 rm opencv_contrib.zip
 
-# set install dir
+# Set install dir
 cd ~/opencv
 mkdir build
 cd build
 
-# run cmake
+# Run cmake
 cmake -D CMAKE_BUILD_TYPE=RELEASE \
 -D CMAKE_INSTALL_PREFIX=/usr/local \
 -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules \
@@ -67,18 +69,17 @@ cmake -D CMAKE_BUILD_TYPE=RELEASE \
 -D OPENCV_GENERATE_PKGCONFIG=ON \
 -D BUILD_EXAMPLES=OFF ..
 
-# run make
+# Run make
 make -j4
 sudo make install
 sudo ldconfig
 
-# cleaning (frees 300 MB)
+# Clean up (frees 300 MB)
 make clean
-sudo apt-get update
+sudo apt update
 sudo sed -ie 's/CONF_SWAPSIZE=3000/CONF_SWAPSIZE=100/' /etc/dphys-swapfile
 sudo /etc/init.d/dphys-swapfile stop
 sudo /etc/init.d/dphys-swapfile start
-
 
 echo "Congratulations!"
 echo "You've successfully installed OpenCV 4.6.0 on your Raspberry Pi 64-bit OS"
